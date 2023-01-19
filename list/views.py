5a7f12e5cc2804116a0f0c7af07dcaba8f90d21e
Kotmin <ptlusty@proton.me>
2023-01-19 20:27:49 +0100

    New templates work translation

--- list/views.py
+++ list/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse # just for debug purposes
+
+
+from django.views.generic.list import ListView
+from .models import Task
+
+#Auto translation part
 from django.utils.translation import gettext as _
+
+
 
 # Create your views here.
 
 
-def taskList(request):
+def throw_HTTP(request):
     """
     Main functionality returns a HTTPresponce
     """
     # Translators: This message shoudl appear only while logged on the main app page
     output = _("Your todo list")
     return HttpResponse(output)
+
+
+class TaskList(ListView):
+    model = Task
+    # req model or qury set
