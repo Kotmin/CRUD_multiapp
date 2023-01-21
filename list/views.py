a6e25f8c6f9804bd241d47e57f4f6801bd8e89a4
Kotmin <ptlusty@proton.me>
2023-01-21 03:11:18 +0100

    range of view

--- list/views.py
+++ list/views.py
@@ -9,7 +9,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 
 #Auto translation part
@@ -38,27 +38,38 @@ class CustomLoginView(LoginView):
     def get_success_url(self) -> str:
         return reverse_lazy('tasks')
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin,ListView):
     model = Task # req model or query set
     context_object_name = 'tasks'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        #context['count'] = context['tasks'].filter(complete=False).count()
+        return context
 
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'list/task.html' # path lib!!!
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin,CreateView):
     model = Task
-    fields = '__all__' # alt =['title','desc]
+    #fields = '__all__' # alt =['title','desc]
+    fields = ['title','description','state']
     success_url = reverse_lazy('tasks')
     
-class TaskUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate,self).form_valid(form)
+    
+class TaskUpdate(LoginRequiredMixin,UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin,DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
