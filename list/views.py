5146265927a464633195e32978485723477d7a34
Kotmin <ptlusty@proton.me>
2023-01-21 03:55:42 +0100

    register form

--- list/views.py
+++ list/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse # just for debug purposes
 
 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+
 from .models import Task
 
 #Auto translation part
@@ -38,6 +42,24 @@ class CustomLoginView(LoginView):
     def get_success_url(self) -> str:
         return reverse_lazy('tasks')
 
+
+class RegisterPage(FormView):
+    template_name = 'list/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self,form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage,self).form_valid(form)
+
+    def get(self,*args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args,**kwargs)
+
 class TaskList(LoginRequiredMixin,ListView):
     model = Task # req model or query set
     context_object_name = 'tasks'
@@ -45,7 +67,11 @@ class TaskList(LoginRequiredMixin,ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
-        #context['count'] = context['tasks'].filter(complete=False).count()
+        def n_deleted(x):
+            if x==0:
+                return False
+            return True
+        # context['count'] = context['tasks'].filter(status=1).count()
         return context
 
 class TaskDetail(DetailView):
