2309e1c3cfa3ab40a24f4a26ad08c9bded60346b
Kotmin <ptlusty@proton.me>
2023-02-06 21:49:50 +0100

    hangman game input

--- list/static/list/js/type_game.js
+++ list/static/list/js/type_game.js
@@ -7,12 +7,13 @@ typingInput = document.querySelector(".typing-input");
 
 
 let text = "Ala ma kota";
+// text = "ala";
 
 
 function randomText(){
     let html ="";
     for(let i = 0; i< text.length;i++){
-        html=`<input type="text" disabled>`;
+        html +=`<input type="text" disabled>`;
     }
     inputs.innerHTML = html;
     
@@ -24,8 +25,19 @@ function initGame(e) {
     let key = e.target.value;
     if(key.match(/^[A-Za-z0-9]+$/)){
         console.log(key);
+        if(text.includes(key)) {
+            for (let i = 0; i < text.length; i++) {
+                if (text[i] === key) {
+                    inputs.querySelectorAll("input")[i].value = key;
+                }
+            }
+        }
+        else{
+            // console.log("letter not found");
+            console.log("letter not found");
+        }
     }
-    
+    typingInput.value ="" 
 }
 //16:15 v
 resetBtn.addEventListener("click",randomText);
